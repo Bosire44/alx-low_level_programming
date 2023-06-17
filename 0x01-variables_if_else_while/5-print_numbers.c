da0a4c7cd1a4a0cfc8b0d7b42aeae492edf43997
Bosire44 <bosire.marketing@gmail.com>
2023-06-16 18:56:45 -0700

    Decimal digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all digit of base 10
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
