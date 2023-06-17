e86b110dc222f37f2c7fb2f861566408543f7d03
Bosire44 <bosire.marketing@gmail.com>
2023-06-16 18:50:11 -0700

    Lower case except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase except q and e
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar(10);
+	
+	return (0);
+}
