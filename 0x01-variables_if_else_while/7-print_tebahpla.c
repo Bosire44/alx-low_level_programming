afae0291ad2ab7152ccf71c99383b2f443162321
Bosire44 <bosire.marketing@gmail.com>
2023-06-16 19:08:20 -0700

    Alphabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet i reverse
+ * Return: 0 if complete
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
