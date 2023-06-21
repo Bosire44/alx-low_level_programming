e9268750ba8f6a51e8a70513d43e6a825831d86a
Bosire44 <bosire.marketing@gmail.com>
2023-06-20 19:59:45 -0700

    Sth

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- print fibonacci
+ * Return: 0 if successful
+ */
+int main(void)
+{
+    int fib[50];
+    fib[0] = 1;  // First Fibonacci number
+    fib[1] = 2;  // Second Fibonacci number
+
+    // Generate Fibonacci numbers
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    // Print the Fibonacci numbers separated by comma and space
+    for (int i = 0; i < n; i++) {
+        printf("%d", fib[i]);
+        if (i != n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
