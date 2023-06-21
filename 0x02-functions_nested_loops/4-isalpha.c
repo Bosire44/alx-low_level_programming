50cf101cc1e908c353ad42ff692a5997fbab7b75
Bosire44 <bosire.marketing@gmail.com>
2023-06-20 17:08:41 -0700

    Charcater is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is alpha
+ * @c: Charcater to be checked
+ * Return: 1 if c is a letter and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}	
