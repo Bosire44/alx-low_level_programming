12ce3b8a0a47f8b0881a257021d63282ddd1091d
Bosire44 <bosire.marketing@gmail.com>
2023-06-20 19:42:05 -0700

    Multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - writes sum of multiples of 3 and 9
+ * Return - the number
+ */
+int main(void)
+{
+	int i,  sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += 1;
+		}
+	}
+	printf("%d\n",sum);
+	return sum;
+}
