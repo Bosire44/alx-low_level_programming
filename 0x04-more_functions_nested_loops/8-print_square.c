91c500601db267b757d1e0d518c14927ce7df94f
Bosire44 <bosire.marketing@gmail.com>
2023-06-23 06:51:23 -0700

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - Print square
+ * @size: size to be printed
+ * Return: I d k
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size < 1)
+		_putchar(10);
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+}
