a7d61e140e00901cd4865caf2bad50c098d9cbef
Bosire44 <bosire.marketing@gmail.com>
2023-06-23 08:07:35 -0700

    Numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - print numbers
+ * @n: printed number
+ * Return: th here
+ */
+void print_number(int n)
+{	
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -1 * n;
+	}
+	if (n < 10)
+	{
+		_putchar(48 + n);
+	}
+	else if ( n < 100)
+	{
+		_putchar(48 + (n / 10));
+		_putchar(48 + (n % 10));
+	}
+	else if (n < 1000)
+	{
+		_putchar(48 + (n / 100));
+		_putchar(48 + (n / 10) % 10);
+		_putchar(48 + (n % 100));
+	}
+	else if ( n < 10000)
+	{
+		_putchar(48 + (n / 1000));
+		_putchar(48 + (n / 100) % 10);
+		_putchar(48 + (n / 10) % 10);
+		_putchar(48 + (n % 1000));
+	}
+}
