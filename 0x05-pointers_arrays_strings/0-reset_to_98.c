a53d6107971065ba1a5832cdc1a5d4cee6e88209
Bosire44 <bosire.marketing@gmail.com>
2023-06-26 09:12:06 -0700

    Change number to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * reset_to_98 - updates to 98
+ * @*n: Target
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
