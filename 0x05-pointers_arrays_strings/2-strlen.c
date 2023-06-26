22eb47b3c7cc6726e263aefd62a0c3ca7d22d80d
Bosire44 <bosire.marketing@gmail.com>
2023-06-26 11:14:34 -0700

    Length os string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - returns len of string
+ * @s: string 
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+	return (i);
+}
