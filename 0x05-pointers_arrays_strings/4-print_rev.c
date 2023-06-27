46d73920901fc18598fa7a4d1a7e2030085aa5e1
Bosire44 <bosire.marketing@gmail.com>
2023-06-26 18:30:33 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - print a sring i reverse
+ * @s - string
+ * Return: nothig
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
