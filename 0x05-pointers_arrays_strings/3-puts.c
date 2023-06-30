6cb15de4e57ae15b038c09cd1187c52b83ceea02
Bosire44 <bosire.marketing@gmail.com>
2023-06-30 00:44:24 -0700

    Puts replica

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: - The string
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
