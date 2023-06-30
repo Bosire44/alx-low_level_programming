f4e7d2eeea68d5ed9b6c0e1ff86dbf020ec5782b
Bosire44 <bosire.marketing@gmail.com>
2023-06-30 00:34:54 -0700

    Calc string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - Find length of a string
+ *@s: The string
+ *Return: The length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
