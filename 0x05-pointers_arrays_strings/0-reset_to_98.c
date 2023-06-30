72a3ba7817f66e1a4994ea721a5ab300e4234437
Bosire44 <bosire.marketing@gmail.com>
2023-06-30 00:21:32 -0700

    Reset value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * reset_to_98 - updates a value of a pointe to 98
+ * @n: The value
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
