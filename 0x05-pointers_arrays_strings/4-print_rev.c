d377af685c2d249e13d1944c02e7099c1fd3aac2
Bosire44 <bosire.marketing@gmail.com>
2023-06-30 00:52:22 -0700

    Print s tring in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string
+ * Return: nothing
+ */
+void print_rev(char* s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+	for (j = i - 1; j >= 0; j--)
+		printf("%c", s[j]);
+	printf("\n");
+}
