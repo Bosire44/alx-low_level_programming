a2df2e7369d4caea710db99fd8cdd0fc52865b20
Bosire44 <bosire.marketing@gmail.com>
2023-07-05 06:30:37 -0700

    Puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: Printed string
+ * Retun: nothing
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
