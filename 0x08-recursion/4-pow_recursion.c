17adb513ea788aae56b88e04e747c01aec4e7976
Bosire44 <bosire.marketing@gmail.com>
2023-07-05 08:10:20 -0700

    Find powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - raise x to y
+ * @x: Number
+ * @y: power
+ * Return: Result
+ */
+int _pow_recursion(int x, int y)
+{
+	int result = 1;
+
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return(x);
+	if (y == 0)
+		return (1);
+	result = x * _pow_recursion(x, y - 1);
+	return (result);
+}
